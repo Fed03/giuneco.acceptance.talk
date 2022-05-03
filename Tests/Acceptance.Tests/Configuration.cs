54a6af2506da4963ee18ac87aa7db746fadcbad9
Federico Teotini <teotini.federico@gmail.com>
2022-05-03 15:56:39 +0200

    test proj

---
+++ Tests/Acceptance.Tests/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Acceptance.Tests;
+
+internal static class Configuration
+{
+    public static IConfiguration Load() => new ConfigurationBuilder()
+                                           .AddJsonFile("appsettings.json", false, true)
+                                           .AddJsonFile("appsettings.local.json", true, true)
+                                           .Build();
+}
